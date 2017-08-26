31af313cb443db665973a45db2d315725e1a991c
Gurpreet Singh <g.singh6343@gmail.com>
2017-08-26 20:28:06 +0530

    Added PID, PPID, Time (Tested)

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -1,24 +1,24 @@
-// exception.cc 
-//	Entry point into the Nachos kernel from user programs.
-//	There are two kinds of things that can cause control to
-//	transfer back to here from user code:
+// exception.cc
+//  Entry point into the Nachos kernel from user programs.
+//  There are two kinds of things that can cause control to
+//  transfer back to here from user code:
 //
-//	syscall -- The user code explicitly requests to call a procedure
-//	in the Nachos kernel.  Right now, the only function we support is
-//	"Halt".
+//  syscall -- The user code explicitly requests to call a procedure
+//  in the Nachos kernel.  Right now, the only function we support is
+//  "Halt".
 //
-//	exceptions -- The user code does something that the CPU can't handle.
-//	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//  exceptions -- The user code does something that the CPU can't handle.
+//  For instance, accessing memory that doesn't exist, arithmetic errors,
+//  etc.
 //
-//	Interrupts (which can also cause control to transfer from user
-//	code into the Nachos kernel) are handled elsewhere.
+//  Interrupts (which can also cause control to transfer from user
+//  code into the Nachos kernel) are handled elsewhere.
 //
 // For now, this only handles the Halt() system call.
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -31,219 +31,232 @@
 #include "addrspace.h"
 //----------------------------------------------------------------------
 // ExceptionHandler
-// 	Entry point into the Nachos kernel.  Called when a user program
-//	is executing, and either does a syscall, or generates an addressing
-//	or arithmetic exception.
+//  Entry point into the Nachos kernel.  Called when a user program
+//  is executing, and either does a syscall, or generates an addressing
+//  or arithmetic exception.
 //
-// 	For system calls, the following is the calling convention:
+//  For system calls, the following is the calling convention:
 //
-// 	system call code -- r2
-//		arg1 -- r4
-//		arg2 -- r5
-//		arg3 -- r6
-//		arg4 -- r7
+//  system call code -- r2
+//    arg1 -- r4
+//    arg2 -- r5
+//    arg3 -- r6
+//    arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//  The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
-//	are in machine.h.
+//  "which" is the kind of exception.  The list of possible exceptions
+//  are in machine.h.
 //----------------------------------------------------------------------
-static Semaphore *readAvail;
-static Semaphore *writeDone;
+static Semaphore* readAvail;
+static Semaphore* writeDone;
 static void ReadAvail(int arg) { readAvail->V(); }
 static void WriteDone(int arg) { writeDone->V(); }
 
-static void ConvertIntToHex (unsigned v, Console *console)
+static void ConvertIntToHex(unsigned v, Console* console)
 {
-   unsigned x;
-   if (v == 0) return;
-   ConvertIntToHex (v/16, console);
-   x = v % 16;
-   if (x < 10) {
-      writeDone->P() ;
-      console->PutChar('0'+x);
-   }
-   else {
-      writeDone->P() ;
-      console->PutChar('a'+x-10);
-   }
+    unsigned x;
+    if (v == 0)
+        return;
+    ConvertIntToHex(v / 16, console);
+    x = v % 16;
+    if (x < 10) {
+        writeDone->P();
+        console->PutChar('0' + x);
+    }
+    else {
+        writeDone->P();
+        console->PutChar('a' + x - 10);
+    }
 }
 
-void
-ExceptionHandler(ExceptionType which)
+void ExceptionHandler(ExceptionType which)
 {
     int type = machine->ReadRegister(2);
     int memval, vaddr, printval, tempval, exp;
-    unsigned printvalus;        // Used for printing in hex
+    unsigned printvalus; // Used for printing in hex
     if (!initializedConsoleSemaphores) {
-       readAvail = new Semaphore("read avail", 0);
-       writeDone = new Semaphore("write done", 1);
-       initializedConsoleSemaphores = true;
+        readAvail = new Semaphore("read avail", 0);
+        writeDone = new Semaphore("write done", 1);
+        initializedConsoleSemaphores = true;
     }
-    Console *console = new Console(NULL, NULL, ReadAvail, WriteDone, 0);;
+    Console* console = new Console(NULL, NULL, ReadAvail, WriteDone, 0);
+    ;
 
     if ((which == SyscallException) && (type == SysCall_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
+        DEBUG('a', "Shutdown, initiated by user program.\n");
+        interrupt->Halt();
     }
     else if ((which == SyscallException) && (type == SysCall_PrintInt)) {
-       printval = machine->ReadRegister(4);
-       if (printval == 0) {
-	  writeDone->P() ;
-          console->PutChar('0');
-       }
-       else {
-          if (printval < 0) {
-	     writeDone->P() ;
-             console->PutChar('-');
-             printval = -printval;
-          }
-          tempval = printval;
-          exp=1;
-          while (tempval != 0) {
-             tempval = tempval/10;
-             exp = exp*10;
-          }
-          exp = exp/10;
-          while (exp > 0) {
-	     writeDone->P() ;
-             console->PutChar('0'+(printval/exp));
-             printval = printval % exp;
-             exp = exp/10;
-          }
-       }
-       // Advance program counters.
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        printval = machine->ReadRegister(4);
+        if (printval == 0) {
+            writeDone->P();
+            console->PutChar('0');
+        }
+        else {
+            if (printval < 0) {
+                writeDone->P();
+                console->PutChar('-');
+                printval = -printval;
+            }
+            tempval = printval;
+            exp = 1;
+            while (tempval != 0) {
+                tempval = tempval / 10;
+                exp = exp * 10;
+            }
+            exp = exp / 10;
+            while (exp > 0) {
+                writeDone->P();
+                console->PutChar('0' + (printval / exp));
+                printval = printval % exp;
+                exp = exp / 10;
+            }
+        }
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
     }
     else if ((which == SyscallException) && (type == SysCall_PrintChar)) {
-	writeDone->P() ;
-        console->PutChar(machine->ReadRegister(4));   // echo it!
-       // Advance program counters.
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        writeDone->P();
+        console->PutChar(machine->ReadRegister(4)); // echo it!
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
     }
     else if ((which == SyscallException) && (type == SysCall_PrintString)) {
-       vaddr = machine->ReadRegister(4);
-       machine->ReadMem(vaddr, 1, &memval);
-       while ((*(char*)&memval) != '\0') {
-	  writeDone->P() ;
-          console->PutChar(*(char*)&memval);
-          vaddr++;
-          machine->ReadMem(vaddr, 1, &memval);
-       }
-       // Advance program counters.
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        vaddr = machine->ReadRegister(4);
+        machine->ReadMem(vaddr, 1, &memval);
+        while ((*(char*)&memval) != '\0') {
+            writeDone->P();
+            console->PutChar(*(char*)&memval);
+            vaddr++;
+            machine->ReadMem(vaddr, 1, &memval);
+        }
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
     }
     else if ((which == SyscallException) && (type == SysCall_PrintIntHex)) {
-       printvalus = (unsigned)machine->ReadRegister(4);
-       writeDone->P() ;
-       console->PutChar('0');
-       writeDone->P() ;
-       console->PutChar('x');
-       if (printvalus == 0) {
-          writeDone->P() ;
-          console->PutChar('0');
-       }
-       else {
-          ConvertIntToHex (printvalus, console);
-       }
-       // Advance program counters.
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        printvalus = (unsigned)machine->ReadRegister(4);
+        writeDone->P();
+        console->PutChar('0');
+        writeDone->P();
+        console->PutChar('x');
+        if (printvalus == 0) {
+            writeDone->P();
+            console->PutChar('0');
+        }
+        else {
+            ConvertIntToHex(printvalus, console);
+        }
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
     }
     else if ((which == SyscallException) && (type == SysCall_GetReg)) {
-       
+        //Implemented By Siddhartha/Jaskirat
 
-       int reg_to_read = (unsigned)machine->ReadRegister(4);
-       int value = (unsigned)machine->ReadRegister(reg_to_read);
+        int reg_to_read = (unsigned)machine->ReadRegister(4);
+        int value = (unsigned)machine->ReadRegister(reg_to_read);
 
-       // Advance program counters.
-       machine->WriteRegister(2, value);
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        machine->WriteRegister(2, value);
+
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
     }
 
     else if ((which == SyscallException) && (type == SysCall_GetPA)) {
-      
-      // Machine::Translate(int virtAddr, int* physAddr, int size, bool writing)
-      // {
+        //Implemented By Siddhartha/Jaskirat
+
         int i;
-        unsigned int vpn, offset;
+        unsigned int virtualPageNumber, offset;
         int virtAddr = machine->ReadRegister(4);
-        int physAddr; 
-        TranslationEntry *entry;
+        int physAddr;
+        TranslationEntry* entry;
         unsigned int pageFrame;
 
-      //     DEBUG('a', "\tTranslate 0x%x, %s: ", virtAddr, writing ? "write" : "read");
-
-      //     // check for alignment errors
-      //     if (((size == 4) && (virtAddr & 0x3)) || ((size == 2) && (virtAddr & 0x1))){
-      //         DEBUG('a', "alignment problem at %d, size %d!\n", virtAddr, size);
-      //         return AddressErrorException;
-      //     }
-
-      //     // we must have either a TLB or a page table, but not both!
-      //     ASSERT(tlb == NULL || KernelPageTable == NULL);
-      //     ASSERT(tlb != NULL || KernelPageTable != NULL);
-
-      //     // calculate the virtual page number, and offset within the page,
-      //     // from the virtual address
-        vpn = (unsigned) virtAddr / PageSize;
-        offset = (unsigned) virtAddr % PageSize;
+        virtualPageNumber = (unsigned)virtAddr / PageSize;
+        offset = (unsigned)virtAddr % PageSize;
 
         if (machine->tlb == NULL) {
-          if(vpn > machine->pageTableSize){
-            machine->WriteRegister(2, -1);
-          }
-          else if(!machine->KernelPageTable[vpn].valid){
-            machine->WriteRegister(2, -1);
-          }
-          else{
-            entry = &(machine->KernelPageTable[vpn]);
+            if (virtualPageNumber > machine->pageTableSize) {
+                machine->WriteRegister(2, -1);
+            }
+            else if (!machine->KernelPageTable[virtualPageNumber].valid) {
+                machine->WriteRegister(2, -1);
+            }
+            else {
+                entry = &(machine->KernelPageTable[virtualPageNumber]);
+                pageFrame = entry->physicalPage;
+                if (pageFrame >= NumPhysPages) {
+                    machine->WriteRegister(2, -1);
+                }
+                else {
+                    physAddr = pageFrame * PageSize + offset;
+                    machine->WriteRegister(2, physAddr);
+                }
+            }
+        }
+        else {
+            for (entry = NULL, i = 0; i < TLBSize; i++)
+                if ((machine->tlb)[i].valid && ((machine->tlb)[i].virtualPage == virtualPageNumber)) {
+                    entry = &(machine->tlb)[i]; // FOUND!
+                    break;
+                }
             pageFrame = entry->physicalPage;
-            if(pageFrame>=NumPhysPages){
-              machine->WriteRegister(2, -1);
+            if (pageFrame >= NumPhysPages) {
+                machine->WriteRegister(2, -1);
             }
-            else{
-               physAddr = pageFrame * PageSize + offset;
-               machine->WriteRegister(2, physAddr);
+            else {
+                physAddr = pageFrame * PageSize + offset;
+                machine->WriteRegister(2, physAddr);
             }
-          }  
-        }
-        else{
-          for (entry = NULL, i = 0; i < TLBSize; i++)
-            if ((machine->tlb)[i].valid && ((machine->tlb)[i].virtualPage == vpn)) {
-              entry = &(machine->tlb)[i];      // FOUND!
-              break;
-            }
-          pageFrame = entry->physicalPage;
-          if(pageFrame>=NumPhysPages){
-            machine->WriteRegister(2, -1);
-          }
-          else{
-             physAddr = pageFrame * PageSize + offset;
-             machine->WriteRegister(2, physAddr);
-          }
         }
 
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
+    }
+    else if ((which == SyscallException) && (type == SysCall_GetPID)) {
+        //Implemented By Gurpreet/Shobhit
+        machine->WriteRegister(2, currentThread -> GetPID());
 
-       // Advance program counters.
-       // machine->WriteRegister(2, value);
-       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
-    } else {
-  printf("Unexpected user mode exception %d %d\n", which, type);
-  ASSERT(FALSE);
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
+    }
+    else if ((which == SyscallException) && (type == SysCall_GetPPID)) {
+        //Implemented By Gurpreet/Shobhit
+        machine->WriteRegister(2, currentThread -> GetPPID());  
+
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
+    }
+    else if ((which == SyscallException) && (type == SysCall_Time)) {
+        //Implemented By Gurpreet/Shobhit
+        machine->WriteRegister(2, stats -> totalTicks);
+
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
+    }
+    else {
+        printf("Unexpected user mode exception %d %d\n", which, type);
+        ASSERT(FALSE);
     }
 }
