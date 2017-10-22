ce24e9920644ffe130d6f39cac651b7c6ee48b07
Gurpreet Singh <g.singh6343@gmail.com>
2017-10-22 21:31:15 +0530

    Replaced with code from branch:gurpreet

--- mips-i386-xgcc/lib/gcc-lib/mips-dec-ultrix42/3.2.3/include/varargs.h
+++ mips-i386-xgcc/lib/gcc-lib/mips-dec-ultrix42/3.2.3/include/varargs.h
@@ -54,7 +54,7 @@ typedef int __builtin_va_alist_t __attribute__((__mode__(__word__)));
 
 /* ??? It would be nice to get rid of the ellipsis here.  It causes
    current_function_varargs to be set in cc1.  */
-#define va_dcl        __builtin_va_alist_t __builtin_va_alist; ...
+#define va_dcl		__builtin_va_alist_t __builtin_va_alist; ...
 
 /* Define __gnuc_va_list, just as in stdarg.h.  */
 
@@ -63,10 +63,10 @@ typedef int __builtin_va_alist_t __attribute__((__mode__(__word__)));
 typedef __builtin_va_list __gnuc_va_list;
 #endif
 
-#define va_start(v)    __builtin_varargs_start((v))
-#define va_end        __builtin_va_end
-#define va_arg        __builtin_va_arg
-#define __va_copy(d, s)    __builtin_va_copy((d),(s))
+#define va_start(v)	__builtin_varargs_start((v))
+#define va_end		__builtin_va_end
+#define va_arg		__builtin_va_arg
+#define __va_copy(d,s)	__builtin_va_copy((d),(s))
 
 /* Define va_list from __gnuc_va_list.  */
 
